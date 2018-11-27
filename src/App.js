335e57fc1c05ccf5be70744766685e188badae83
Miriam Nakiwuge <nakiwuge@gmail.com>
2018-11-27 12:16:18 +0300

    ch(routing) setUp react router version 4
    - add AppRoutes to APP.js
    
    [Deliver #162190004]

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import { Provider } from "react-redux";
 import store from "./store";
+import AppRoutes from "./routes";
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <h1>home</h1>
+        <AppRoutes/>
       </Provider>
     );
   }
